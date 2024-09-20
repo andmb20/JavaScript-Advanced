2f8d087976e2c5cb18953bb2ce83c2e08b4c8b85
andre <andremorais0000@gmail.com>
2024-09-19 21:54:15 -0300

    APIs

---
+++ JSON/api.js
@@ -0,0 +1,15 @@
+/*
+    API, API REST, API RESTful
+
+    API (Application Programming Interface) -> Interface de Programa de Aplicações.
+    - É um conjunto de regras que permite que diferntes sistemas ou aplicações se comuniquem entre si. Ela define métodos e formatos de dados para que informações possam ser trocadas de maneira padronizada.
+
+    API REST (Representational State Transfer) -> Transferência de Estado Representacional.
+    - É um estilo arquitetural para a construção de APIs, baseada em princípios que incluem:
+        -> Comunicação sem estado (stateless), onde cada solicitação do Cliente para o Servidor deve conter todas as      informações necessárias.
+        -> Uso de verbos HTTP (GET, POST, PUT, DELETE) para realizar operações.
+        -> Representação dos recursos atraves de URLs.
+
+    API RESTful
+    - É uma implementação que segue os princípios REST. Para ser considerada RESTful, a API deve seguir as restrições arquitetônicas do REST, como uniformidade na interface, sem estado, e a capacidade de cache.
+*/
